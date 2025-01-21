9bc1181599667759e8a80c79dab02cdb58bc4dc1
iO <senpai9464@yandex.ru>
2025-01-21 16:24:47 +0300

    upd: add onSubmittong form user info

--- src/modules/profile/Module.tsx
+++ src/modules/profile/Module.tsx
@@ -1,30 +1,14 @@
 import { ModuleFlow } from '@/components/layouts/ModuleFlow';
 import { useBackButton } from '@/shared/hooks/useBackButton';
-import { initDataRaw } from '@telegram-apps/sdk-react';
 import { PaidSubscription } from './components/PaidSubscription';
 import { ProfileCard } from './components/ProfileCard';
-import { ProfileSkeleton } from './components/skeletons/ProfileSkeleton';
-import { useAuth } from './hooks/useAuth';
 
 export const ProfileModule = () => {
   useBackButton(true, () => alert('back'));
-  const dataRaw = initDataRaw();
-  const { data, isSuccess, isLoading, isError } = useAuth(dataRaw ?? '');
 
   return (
     <ModuleFlow>
-      {isLoading && <ProfileSkeleton />}
-
-      {isSuccess && data && (
-        <ProfileCard
-          userName={data.user.username}
-          firstName={data.user.username}
-          title={data.user.infos?.[0]?.title ?? ''}
-          description={data.user.infos?.[0]?.content ?? ''}
-        />
-      )}
-
-      {isError && <p>{dataRaw}</p>}
+      <ProfileCard />
       <PaidSubscription />
     </ModuleFlow>
   );
--- src/modules/profile/components/ProfileCard.tsx
+++ src/modules/profile/components/ProfileCard.tsx
@@ -4,26 +4,42 @@ import { ReactComponent as Clear } from '@/assets/svg/xIcon.svg';
 import { ConnectWalletButton } from '../widgets/ConnetWalletButton';
 
 import { ReactComponent as Basket } from '@/assets/svg/basketIcon.svg';
+import { Alert } from '@heroui/react';
+import { initDataRaw, initDataUser } from '@telegram-apps/sdk-react';
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
+import { useAddInfo } from '../hooks/useAddInfo';
+import { useAuth } from '../hooks/useAuth';
+import { ProfileSkeleton } from './skeletons/ProfileSkeleton';
 
-type Props = {
-  userName: string;
-  firstName: string;
-} & InformationContainerProps;
+export const ProfileCard = () => {
+  const dataRaw = initDataRaw();
+  const { data, isSuccess, isLoading, isError, refetch } = useAuth(dataRaw ?? '');
 
-export const ProfileCard: FC<Props> = ({ userName, firstName, title, description }) => (
-  <div className='relative grid h-fit w-full auto-rows-max gap-4 rounded-5xl bg-foreground p-3.5'>
-    <Header userName={userName} firstName={firstName} />
+  return (
+    <>
+      {isSuccess && data && (
+        <div className='relative grid h-fit w-full auto-rows-max gap-4 rounded-5xl bg-foreground p-3.5'>
+          <Header userName={data.user.username} firstName={data.user.username} />
 
-    <div className='grid w-full auto-rows-max gap-4'>
-      {title && description ? <InformationContainer title={title} description={description} /> : <FormInformation />}
-      <ButtonsContainer />
-    </div>
+          <div className='grid w-full auto-rows-max gap-4'>
+            {data?.user.infos && data.user.infos.length > 0 && isSuccess ? (
+              <InformationContainer title={data.user.infos?.[0]?.title} description={data.user.infos?.[0]?.content} />
+            ) : (
+              <FormInformation refetch={refetch} />
+            )}
+            <ButtonsContainer />
+          </div>
 
-    <ConnectWalletButton />
-  </div>
-);
+          <ConnectWalletButton />
+        </div>
+      )}
+
+      {isLoading && <ProfileSkeleton />}
+      {isError && <Alert description={'Error'} title='dont know' color='warning' />}
+    </>
+  );
+};
 
 const Header: FC<{ userName: string; firstName: string }> = ({ userName, firstName }) => (
   <div className='flex h-fit w-full flex-row items-center justify-between'>
@@ -60,30 +76,50 @@ const ButtonsContainer = () => (
   </div>
 );
 
-const FormInformation = () => {
-  const { formState, register, handleSubmit } = useForm<{ description: string }>({
+type FormInformation = {
+  title: string;
+  content: string;
+};
+
+const FormInformation = ({ refetch }: { refetch: () => void }) => {
+  const { formState, register, handleSubmit, reset } = useForm<FormInformation>({
     defaultValues: {
-      description: '',
+      title: '',
+      content: '',
     },
     mode: 'onChange',
   });
+  const { mutate } = useAddInfo(refetch);
+  const UserInitData = initDataUser();
 
-  const onSubmit = ({ description }: { description: string }) => {
-    console.log(description);
+  const onSubmit = ({ title, content }: FormInformation) => {
+    if (UserInitData) mutate({ userId: UserInitData.id, title, content });
   };
 
   return (
     <div className='flex h-fit w-full flex-col gap-2'>
-      <h2 className='text-xl font-medium text-muted opacity-60'>Нет информации</h2>
+      <input
+        className={`h-10 w-full p-2.5 text-xl font-medium opacity-60 outline-none ${
+          formState.errors.title ? 'text-[#E91E65] placeholder:text-[#E91E65]' : 'text-muted placeholder:text-muted'
+        }`}
+        placeholder='Название информации'
+        {...register('title', {
+          required: 'Это поле обязательно для заполнения',
+          maxLength: {
+            value: 24,
+            message: 'Максимальная длина текста — 25 символов',
+          },
+        })}
+      />
 
       <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-[1fr_auto] gap-2'>
         <div className='flex flex-col gap-1'>
           <textarea
             className={`h-28 w-full rounded-xs p-2.5 text-secondary-foreground outline-none ${
-              formState.errors.description ? 'border-2 border-[#E91E65] bg-[#E91E65]/15' : 'bg-muted-secondary'
+              formState.errors.content ? 'border-2 border-[#E91E65] bg-[#E91E65]/10' : 'bg-muted-secondary'
             }`}
             placeholder='Данное поле будет по умолчанию отображаться в профиле'
-            {...register('description', {
+            {...register('content', {
               required: 'Это поле обязательно для заполнения',
               maxLength: {
                 value: 500,
@@ -92,7 +128,7 @@ const FormInformation = () => {
             })}
           />
 
-          {formState.errors.description && <span className='text-sm text-[#E91E65]'>{formState.errors.description.message}</span>}
+          {formState.errors.content && <span className='text-sm text-[#E91E65]'>{formState.errors.content.message}</span>}
         </div>
 
         <div className='flex h-full w-fit flex-col gap-1 justify-self-start'>
@@ -100,7 +136,7 @@ const FormInformation = () => {
             <Save className='h-5 w-5 stroke-foreground stroke-[1.2]' />
           </button>
 
-          <button type='reset' className='h-fit w-fit rounded-xxs bg-[#E91E65] p-2'>
+          <button className='h-fit w-fit rounded-xxs bg-[#E91E65] p-2' onClick={() => reset()}>
             <Clear className='h-5 w-5 stroke-foreground stroke-[1.2]' />
           </button>
         </div>
